bb0d1ce431597dbe5155a1862c854b9e33aaba4d
Aaron Patten <aaron@nebuloninc.com>
2020-06-22 13:50:33 -0400

    remove files

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#This doesn't seem to work with the YCSB stuff. Assume its calling python by a hard coded path
-#echo "alias python=/usr/bin/python2.7" >> /root/.bashrc
-
-#Copy python2.7 to python to make ycsb work
-#cp /usr/bin/python2.7 /usr/bin/python
